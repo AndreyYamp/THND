0284bf92c54a9dd2d084d1b78ef90ed798a56746
Andrey <andrey.yampolskiy@gmail.com>
2016-08-25 00:13:30 +0300

    Changed the library to UIP Ethernet. Added client part. Now the web page refreshes automaticaly every 30 sec.

--- web_server_dig_temp/web_server_dig_temp.ino
+++ web_server_dig_temp/web_server_dig_temp.ino
@@ -1,6 +1,5 @@
 // Include libraries for ethernet shield
-#include "etherShield.h"
-#include "ETHER_28J60.h"
+#include <UIPEthernet.h>
 // Include libraries for digital temperature sensor
 #include <OneWire.h>
 #include <DallasTemperature.h>
@@ -12,36 +11,77 @@ OneWire oneWire(ONE_WIRE_BUS);
 // Pass our oneWire reference to Dallas Temperature. 
 DallasTemperature sensors(&oneWire);
 
-static uint8_t mac[6] = {0x54, 0x55, 0x58, 0x10, 0x00, 0x24};   // this just needs to be unique for your network, 
-                                                                // so unless you have more than one of these boards
-                                                                // connected, you should be fine with this value.
-                                                           
-static uint8_t ip[4] = {192, 168, 0, 115};                       // the IP address for your board. Check your home hub
-                                                                // to find an IP address not in use and pick that
-                                                                // this or 10.0.0.15 are likely formats for an address
-                                                                // that will work.
-                                                                // my router gives IPs from 192.168.0.100-199
-
-static uint16_t port = 80;                                      // Use port 80 - the standard for HTTP
-
-ETHER_28J60 ethernet;
-void setup() {
-  Serial.begin(9600);
-  ethernet.setup(mac, ip, port); // setup connection
+// Enter a MAC address and IP address for your controller below.
+// The IP address will be dependent on your local network:
+byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
   
-  // Start up the library
+IPAddress ip(192,168,0,115);
+
+// Initialize the Ethernet server library
+// with the IP address and port you want to use 
+// (port 80 is default for HTTP):
+EthernetServer server(80);
+
+void setup() {
+ // Open serial communications:
+  Serial.begin(9600);
+  // start the Ethernet connection and the server:
+  Ethernet.begin(mac, ip);
+  server.begin();
+  Serial.print("server is at ");
+  Serial.println(Ethernet.localIP());
+
   sensors.begin();
 }
 
 void loop() {
   double temp;
-  sensors.requestTemperatures(); // Send the command to get temperatures
-  temp = sensors.getTempCByIndex(0);
-  if (ethernet.serviceRequest())
-  {
-    ethernet.print("<H3>Температура на датчике: </H3>");
-    ethernet.print(temp);
-    ethernet.respond();
+
+  // listen for incoming clients
+  EthernetClient client = server.available();
+  if (client) {
+    Serial.println("new client");
+    // an http request ends with a blank line
+    boolean currentLineIsBlank = true;
+    while (client.connected()) {
+      if (client.available()) {
+        char c = client.read();
+        Serial.write(c);
+        // if you've gotten to the end of the line (received a newline
+        // character) and the line is blank, the http request has ended,
+        // so you can send a reply
+        if (c == '\n' && currentLineIsBlank) {
+          // send a standard http response header
+          client.println("HTTP/1.1 200 OK");
+          client.println("Content-Type: text/html");
+          client.println("Connection: close");  // the connection will be closed after completion of the response
+          client.println("Refresh: 30");  // refresh the page automatically every 5 sec
+          client.println();
+          client.println("<!DOCTYPE HTML>");
+          client.println("<html>");
+          // output the value of digital temperature sensor
+          sensors.requestTemperatures(); // Send the command to get temperatures
+          temp = sensors.getTempCByIndex(0);
+          client.print("Текущая температура (Current temperature) ");
+          client.print(temp);
+          client.println("<br />");       
+          client.println("</html>");
+          break;
+        }
+        if (c == '\n') {
+          // you're starting a new line
+          currentLineIsBlank = true;
+        } 
+        else if (c != '\r') {
+          // you've gotten a character on the current line
+          currentLineIsBlank = false;
+        }
+      }
+    }
+    // give the web browser time to receive the data
+    delay(1);
+    // close the connection:
+    client.stop();
+    Serial.println("client disonnected");
   }
-  delay(100);
 }
